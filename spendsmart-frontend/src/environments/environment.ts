f81afe8d39f2295a6ee203b767864163c191baa2
Harsh-Raj-Singh25 <harshrajsingh252@gmail.com>
2026-05-16 13:41:05 +0530

    [Harsh Raj Singh] : Fix production API URL

--- spendsmart-frontend/src/environments/environment.ts
+++ spendsmart-frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080',
+  production: true,
+  apiUrl: 'http://spend-smart.duckdns.org:8080',
   googleClientId: '791118802097-brtckvmholg9677jn55bque28dlkm4o7.apps.googleusercontent.com'
 };
